f17f3451919e017b3061c163cdcde532a69b6b80
Ang Qi Jun <qang@Angs-MacBook-Air.local>
2024-02-07 15:29:17 +0800

    style activity card

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ShareIcon from '@mui/icons-material/Share';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -6,38 +7,80 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import '../App.css'
-import picture from "../resources/cute anime girl.webp"
 import Activity from "../interfaces/Activity";
+import {CardHeader, Collapse, IconButton, Stack} from "@mui/material";
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import {More} from "@mui/icons-material";
+// import picture from "../resources/cute anime girl.webp"
 
-export default function ActivityCard({ id, title, overview, body, image, manpower_needed, location, time_start, time_end, created_at, updated_at }:Activity) {
+
+export default function ActivityCard(props: {
+    activity: Activity
+}) {
+    const activity = props.activity;
+    const id = activity.id;
+    const image = activity.image == null ? undefined : activity.image;
+    const [bodyVisible, setBodyVisible] = useState(false);
+
+    const toggleBody = () => {
+        setBodyVisible(!bodyVisible);
+    };
     return (
-        <div>
-            <Card sx={{
-                maxWidth: 300
-            }}>
+        <div id={id.toString()}>
+            <Card sx={{minWidth: 280, maxWidth: 280, minHeight: 500, maxHeight: 530}}>
+                <CardHeader
+                    subheader={
+                        <Typography variant="body2" sx={{ fontSize: '0.8rem' }}>
+                            Created: {activity.created_at} <br/>
+                            Updated: {activity.updated_at - activity.created_at} ago
+                        </Typography>
+                    }
+                />
                 <CardMedia
                     sx={{ height: 200 }}
-                    image={picture}
+                    image={image}
                     component="img"
-                    title={title}
+                    alt={activity.title}
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {overview}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {time_start} to {time_end}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        manpower: {manpower_needed}
-                    </Typography>
+                    <CardActions title={activity.title}>
+                        <Typography variant="h6" component="div">
+                            {activity.title}
+                        </Typography>
+                        <IconButton sx={{right: 1}} onClick={toggleBody}><More /></IconButton>
+                    </CardActions>
+                    <Collapse in={bodyVisible} timeout="auto" unmountOnExit>
+                        <CardContent style={{maxHeight: 85, overflow: 'auto'}}>
+                            <Typography>
+                                {activity.body}
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                    {!bodyVisible &&
+                        <div>
+                            <Typography variant="body1" color="text.secondary">
+                                {activity.overview}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                From {activity.time_start} to {activity.time_end}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Volunteers needed: {activity.manpower_needed}
+                            </Typography>
+                            <Typography variant="body2">
+                                <a href="">{activity.location}</a>
+                            </Typography>
+                        </div>
+                    }
                 </CardContent>
-                <CardActions>
-                    <a href="">{location}</a>
-                    <Button size="small">Apply</Button>
+                <CardActions sx={{ justifyContent: 'center' }}>
+                    <Stack direction="row" spacing={2}>
+                        <Link to={`/activities/${activity.id}`}>
+                            <Button variant="contained" size="small">Learn more</Button>
+                        </Link>
+                        <Button variant="contained" size="small" startIcon={<ShareIcon />}>Share</Button>
+                    </Stack>
                 </CardActions>
             </Card>
         </div>
